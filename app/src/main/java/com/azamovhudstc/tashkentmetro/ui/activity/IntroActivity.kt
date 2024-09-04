ea21cce3679e799c55b0ca82e0240440e37aa191
asadbekturgunboyev <murodaka99@gmail.com>
2024-09-05 00:17:06 +0500

    some change configuration

---
+++ app/src/main/java/com/azamovhudstc/tashkentmetro/ui/activity/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.azamovhudstc.tashkentmetro.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.azamovhudstc.tashkentmetro.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
