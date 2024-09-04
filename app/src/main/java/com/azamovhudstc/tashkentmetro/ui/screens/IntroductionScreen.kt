c8b318796f33b9b28f8a71a19389be38afb44bad
professorDeveloper <azamovhud007@outlook.com>
2024-09-04 22:50:10 +0500

    Probably Nothing

---
+++ app/src/main/java/com/azamovhudstc/tashkentmetro/ui/screens/IntroductionScreen.kt
@@ -0,0 +1,12 @@
+package com.azamovhudstc.tashkentmetro.ui.screens
+
+import com.azamovhudstc.tashkentmetro.databinding.IntroductionScreenBinding
+import com.azamovhudstc.tashkentmetro.utils.BaseFragment
+
+class IntroductionScreen :
+    BaseFragment<IntroductionScreenBinding>(IntroductionScreenBinding::inflate) {
+    override fun onViewCreate() {
+
+    }
+
+}
