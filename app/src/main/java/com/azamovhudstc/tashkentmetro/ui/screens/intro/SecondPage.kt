42b717150a3cad81f4b0c625b81f97ac3cf878a9
professorDeveloper <azamovhud007@outlook.com>
2024-09-08 02:41:00 +0500

    Profile page is making

--- app/src/main/java/com/azamovhudstc/tashkentmetro/ui/screens/intro/SecondPage.kt
+++ app/src/main/java/com/azamovhudstc/tashkentmetro/ui/screens/intro/SecondPage.kt
@@ -1,17 +1,29 @@
 package com.azamovhudstc.tashkentmetro.ui.screens.intro
 
 import androidx.navigation.fragment.findNavController
-import com.azamovhudstc.infinityinsurance.utils.animationTransaction
-import com.azamovhudstc.infinityinsurance.utils.setSafeOnClickListener
 import com.azamovhudstc.tashkentmetro.R
 import com.azamovhudstc.tashkentmetro.databinding.SecondPageBinding
 import com.azamovhudstc.tashkentmetro.utils.BaseFragment
+import com.azamovhudstc.tashkentmetro.utils.animationTransaction
+import com.azamovhudstc.tashkentmetro.utils.setSafeOnClickListener
+import com.azamovhudstc.tashkentmetro.utils.slideTop
+import com.azamovhudstc.tashkentmetro.utils.slideUp
+import com.azamovhudstc.tashkentmetro.utils.visible
 
 
 class SecondPage : BaseFragment<SecondPageBinding>(SecondPageBinding::inflate) {
 
     override fun onViewCreate() {
+        binding.apply {
+            nextBtn.slideUp(1000, 0)
+            gradientMaterialButton.slideTop(1000, 0)
+            infoBtn.setSafeOnClickListener {
+
+            }
+        }
         binding.nextBtn.setSafeOnClickListener {
+            binding.apply {
+            }
             findNavController().navigate(R.id.thirdPage, null, animationTransaction().build())
         }
     }
