4c3248b0878abb21e28497b2c6eb835d94955abb
professorDeveloper <azamovhud007@outlook.com>
2024-09-25 23:06:10 +0500

    fixed home screen collapsing

--- app/src/main/java/com/azamovhudstc/tashkentmetro/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/azamovhudstc/tashkentmetro/ui/screens/home/HomeScreen.kt
@@ -1,15 +1,12 @@
 package com.azamovhudstc.tashkentmetro.ui.screens.home
 
-import android.annotation.SuppressLint
-import android.view.View
-import androidx.core.content.ContextCompat
-import com.azamovhudstc.tashkentmetro.R
 import com.azamovhudstc.tashkentmetro.databinding.HomeScreenBinding
-import com.azamovhudstc.tashkentmetro.utils.*
-import com.google.android.material.appbar.AppBarLayout
+import com.azamovhudstc.tashkentmetro.utils.BaseFragment
+import com.azamovhudstc.tashkentmetro.utils.LocalData
+import com.azamovhudstc.tashkentmetro.utils.slideUp
 
-class HomeScreen : BaseFragment<HomeScreenBinding>(HomeScreenBinding::inflate),
-    AppBarLayout.OnOffsetChangedListener {
+class HomeScreen : BaseFragment<HomeScreenBinding>(HomeScreenBinding::inflate)
+     {
 
     private val adapter by lazy { HomeAdapter() }
     private var isCollapsed = false
@@ -19,7 +16,6 @@ class HomeScreen : BaseFragment<HomeScreenBinding>(HomeScreenBinding::inflate),
 
     override fun onViewCreate() {
         screenWidth = resources.displayMetrics.widthPixels.toFloat()
-        binding.appBarLayout.addOnOffsetChangedListener(this@HomeScreen)
 
         binding.textView.slideUp(900, 0)
         binding.relativeLayout.slideUp(900, 0)
@@ -29,39 +25,5 @@ class HomeScreen : BaseFragment<HomeScreenBinding>(HomeScreenBinding::inflate),
         adapter.submitList(LocalData.trainsStatusList)
     }
 
-    @SuppressLint("ResourceType")
-    override fun onOffsetChanged(appBar: AppBarLayout, i: Int) {
-        if (mMaxScrollSize == 0) mMaxScrollSize = appBar.totalScrollRange
-        val percentage = Math.abs(i) * 100 / mMaxScrollSize
 
-
-
-        binding.animecover.visibility =
-            if (binding.animecover.scaleX == 0f) View.GONE else View.VISIBLE
-        binding.animecover.visibility =
-            if (binding.animecover.scaleX == 0f) View.GONE else View.VISIBLE
-
-
-        if (percentage >= percent && !isCollapsed) {
-            isCollapsed = true
-            requireActivity().window.statusBarColor = ContextCompat.getColor(requireActivity(), R.color.main_background)
-
-            binding.mediaTitleContainer.slideStart(600,0)
-            binding.mediaTitleContainer.visible()
-        }
-        if (percentage <= percent && isCollapsed) {
-            isCollapsed = false
-            requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.main_background)
-            binding.mediaTitleContainer.alphaAnim()
-            binding.mediaTitleContainer.gone()
-
-
-        }
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        binding.mediaTitleContainer.gone()
-    }
 }
