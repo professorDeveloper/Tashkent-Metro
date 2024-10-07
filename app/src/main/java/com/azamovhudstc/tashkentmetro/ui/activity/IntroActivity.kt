41369d2dd91127c3f8be754c2abb5ff3f3340e93
professorDeveloper <azamovhud007@outlook.com>
2024-10-07 14:53:14 +0500

    Android Splash Bug Fixed

--- app/src/main/java/com/azamovhudstc/tashkentmetro/ui/activity/IntroActivity.kt
+++ app/src/main/java/com/azamovhudstc/tashkentmetro/ui/activity/IntroActivity.kt
@@ -1,32 +1,58 @@
 package com.azamovhudstc.tashkentmetro.ui.activity
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.azamovhudstc.tashkentmetro.data.local.shp.AppReference
+import androidx.lifecycle.Observer
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
+import com.azamovhudstc.tashkentmetro.R
 import com.azamovhudstc.tashkentmetro.databinding.ActivityIntroBinding
-import com.azamovhudstc.tashkentmetro.utils.ViewUtils
 import com.azamovhudstc.tashkentmetro.utils.initActivity
+import com.azamovhudstc.tashkentmetro.utils.visible
+import com.azamovhudstc.tashkentmetro.viewmodel.imp.IntroViewModelImpl
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class IntroActivity : AppCompatActivity() {
 
+    private val model by viewModels<IntroViewModelImpl>()
     private lateinit var viewBinding: ActivityIntroBinding
-    @Inject
-    lateinit var userPreferenceManager: AppReference
+    private lateinit var host: NavHostFragment
+    private lateinit var graph: NavGraph
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         viewBinding = ActivityIntroBinding.inflate(layoutInflater)
+
+
+        host = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        graph = host.navController.navInflater.inflate(R.navigation.intro_graph)
+        model.loginScreenLiveData.observe(this@IntroActivity, getStartedScreenObserver)
+        model.homeScreenLiveData.observe(this@IntroActivity, openHomeScreenObserver)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            model.checkPage()
+        }else {
+            graph.setStartDestination(R.id.splashPage)
+            viewBinding.navHost.visible()
+        }
         setContentView(viewBinding.root)
         initActivity(this)
 
 
-
     }
 
-
-
+    private val openHomeScreenObserver = Observer<Unit> {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    private val getStartedScreenObserver = Observer<Unit> {
+        viewBinding.navHost.visible()
+        graph.setStartDestination(R.id.firstPage)
+    }
 }
